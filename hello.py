e2b04f13f62350615ae29cd010722baed76e6642
KERVY MANIEGO <kervymaniego@gmail.com>
2024-06-14 19:19:19 +0800

    initial push

---
+++ hello.py
@@ -0,0 +1,13 @@
+# instead of .class use type()
+# X = "five"
+# x = 5
+# print(X)
+# if 5 > 2:
+#     print("5 is greater")
+#     print("2 is lesser")
+
+def newfunc(x):
+    if x > 3:
+        print(f"yep {x} is greater")
+
+newfunc(5)
